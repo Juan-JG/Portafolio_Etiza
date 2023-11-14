e8785f2a51655123e415f1a2b9223e0407be6104
Juan Jaña González <54220149+Juan-JG@users.noreply.github.com>
2023-11-14 17:21:01 -0300

    Added media queries for mobile
    Finishing

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const navEl = document.querySelector('.navbar');
 const navLinks = document.querySelectorAll('.nav-link');
 const toggler = document.querySelector('.fa-bars');
-const logo = document.querySelector('.navbar_icon');
+const banner = document.querySelector('.banner--home');
+const bannerText = document.querySelector('.banner-texto');
+const bannerColor = document.querySelector('.navbar-toggler-icon');
+const navbarIcon = document.querySelector('.navbar_icon');
+
+// Function to toggle the solid color background of the banner
+function toggleBannerBackground() {
+    banner.classList.toggle('solid-background');
+    bannerText.classList.toggle('color-text');
+}
+
+// Event listener for the toggler button
+bannerColor.addEventListener('click', toggleBannerBackground);
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scrolled');
         navLinks.forEach(link => link.classList.add('nav-dark'));
         toggler.classList.add('icon-dark');
-        logo.classList.add('navbar_icon--scroll');
+        navbarIcon.classList.add('navbar_icon--scroll');
     } else {
         navEl.classList.remove('navbar-scrolled');
         navLinks.forEach(link => link.classList.remove('nav-dark'));
         toggler.classList.remove('icon-dark');
-        logo.classList.remove('navbar_icon--scroll');
+        navbarIcon.classList.remove('navbar_icon--scroll');
     }
 });
 
