06e02e15b1841489998547b790b4bccab4104a5a
Juan Jaña González <54220149+Juan-JG@users.noreply.github.com>
2023-11-14 15:00:54 -0300

    Navbar Icon
    Added navbar icon transition when scrolling

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const navEl = document.querySelector('.navbar');
 const navLinks = document.querySelectorAll('.nav-link');
 const toggler = document.querySelector('.fa-bars');
+const logo = document.querySelector('.navbar_icon');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scrolled');
         navLinks.forEach(link => link.classList.add('nav-dark'));
         toggler.classList.add('icon-dark');
+        logo.classList.add('navbar_icon--scroll');
     } else {
         navEl.classList.remove('navbar-scrolled');
         navLinks.forEach(link => link.classList.remove('nav-dark'));
         toggler.classList.remove('icon-dark');
+        logo.classList.remove('navbar_icon--scroll');
     }
 });
 
